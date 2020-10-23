09b9247fd3159846c2ab5dc491329ab3627e3607
juanMluduena <juanmluduena1@gmail.com>
2020-10-23 11:03:51 -0300

    Added models and admin
    Basic models and admin functions, i still cant figurate out what to do with dates, but i think im getting there. ah escribia en ingles XD. Lo tire en main xq es muy basico esto

--- tpSpecial/hostapp/admin.py
+++ tpSpecial/hostapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from hostapp.models import Property, Service, Reservation
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "max_persons", "bedrooms", "bathrooms", "beds", "price_per_day")
+    search_fields = ("title", "category", "max_persons", "bedrooms", "bathrooms", "beds", "price_per_day")
+    list_filter = ("category", "max_persons", "bedrooms", "bathrooms", "beds", "price_per_day")
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", )
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("property", "user", "final_price")
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Reservation, ReservationAdmin)
--- tpSpecial/hostapp/models.py
+++ tpSpecial/hostapp/models.py
@@ -1,3 +1,46 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+# No se si poner reservation o booking,
+
+
+class Property(models.Model):
+    # settings.AUTH_USER_MODEL --> referencia al modelo User base de Django
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # verbose_name --> es el nombre con el que va a aparecer en el admin
+    city = models.CharField(max_length=50, verbose_name="Ciudad")
+    # https://docs.djangoproject.com/en/1.10/ref/validators/
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
+    description = models.CharField(max_length=500, verbose_name="Descripcion de la Ficha")
+    title = models.CharField(max_length=20, verbose_name="Titulo de la Ficha")
+    max_persons = models.PositiveIntegerField(verbose_name="Maximo personas", default=1)
+    bedrooms = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name="Dormitorios", default=1)
+    beds = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name="Camas", default=1)
+    bathrooms = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name="Baños", default=1)
+    image = models.ImageField(null=True)
+    # periodoFechasAlquiler(array)
+    # servicios(array)
+    # esto se llama choice, el primer dato sale en la bd, el segundo es con el que se trabaja (rezemos)
+    CATEGORY_CHOICES = (
+        ('HAB PRIV', 'Habitación Privada'),
+        ('CA ENT', 'Casa Entera')
+    )
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+
+# https://gist.github.com/bradmontgomery/1c52b799c4ad274e0cbdd012a8b18f10 Discutir si esto es usable.
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.ImageField(null=True)
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    final_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
+    # periodoFechasAlquiler(array)
+
+# TODO Falta agregar el array de fechas y de servicios, decidir tamaño de imagenes
