ad208002a24780f396c1b46c0e9ddc5e10ce9cf7
juanMluduena <juanmluduena1@gmail.com>
2020-10-27 21:47:16 -0300

    Push de stashed changes?
    Creo que se arreglo = D
    :ant:

--- tpSpecial/hostapp/models.py
+++ tpSpecial/hostapp/models.py
@@ -20,14 +20,58 @@ class Property(models.Model):
     beds = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name="Camas", default=1)
     bathrooms = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name="Baños", default=1)
     image = models.ImageField(null=True)
+<<<<<<< Updated upstream
     # periodoFechasAlquiler(array)
     # servicios(array)
     # esto se llama choice, el primer dato sale en la bd, el segundo es con el que se trabaja (rezemos)
-    CATEGORY_CHOICES = (
-        ('HAB PRIV', 'Habitación Privada'),
-        ('CA ENT', 'Casa Entera')
+=======
+    # esto se llama choice, el primer dato se guarda en la bd, el segundo es el q se muestra en el admin al addear
+    SERVICE_CHOICES = (
+        ('Se admiten mascotas', 'Admiten mascotas'),
+        ('Lugar de estacionamiento privado', 'Estacionamiento privado'),
+        ('Lugar de estacionamiento gratuito techado', 'Estacionamiento gratuito techado'),
+        ('Lugar de estacionamiento gratuito a la calle', 'Estacionamiento gratuito a la calle'),
+        ('Internet', 'Internet'),
+        ('Apto para familias y niños', 'Familias y niños'),
+        ('Televisor', 'Televisor'),
+        ('Elementos basicos para la playa', 'Elementos de playa'),
+        ('Lavarropas', 'Lavarropas'),
+        ('Servicios básicos de higiene', 'Servicios básicos de higiene'),
+        ('Agua caliente', 'Agua caliente'),
+        ('Aire acondicionado', 'Aire acondicionado'),
+        ('Calefacción', 'Calefacción'),
+        ('Elementos de cocina basicos', 'Cocina basica'),
+        ('Microondas', 'Microondas'),
+        ('Desayuno incluido', 'Desayuno incluido'),
+        ('Patio trasero', 'Patio trasero'),
+        ('Patio delantero', 'Patio delantero'),
+        ('Balcón', 'Balcón'),
+        ('Parrilla', 'Parrilla'),
+        ('Frente a la playa', 'Frente a la playa'),
+        ('A pocas cuadras de la playa', 'Cercano a la playa'),
+        ('Botiquin', 'Botiquin'),
+        ('Alarma', 'Alarma'),
+        ('Detector de humo', 'Detector de humo'),
+        ('Detector de monoxido de carbono', 'Detector monoxido de carbono'),
+        ('Barrio privado', 'Barrio privado')
     )
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    # https://pypi.org/project/django-multiselectfield/
+    services = MultiSelectField(choices=SERVICE_CHOICES, min_choices=1, default="No tiene servicios")
+>>>>>>> Stashed changes
+    CATEGORY_CHOICES = (
+        ('Bungaló', 'Bungaló'),
+        ('Cabaña', 'Cabaña'),
+        ('Casa Comunal', 'Casa Comunal'),
+        ('Casa de Huéspedes', 'Casa de Huéspedes'),
+        ('Casa Entera', 'Casa Entera'),
+        ('Chalé', 'Chalé'),
+        ('Departamento', 'Departamento'),
+        ('Duplex', 'Duplex'),
+        ('Loft', 'Loft'),
+        ('Mansión', 'Mansión'),
+        ('Villa Residencial', 'Villa Residencial'),
+    )
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
 
 # https://gist.github.com/bradmontgomery/1c52b799c4ad274e0cbdd012a8b18f10 Discutir si esto es usable.
 
@@ -40,7 +84,15 @@ class Service(models.Model):
 class Reservation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
+<<<<<<< Updated upstream
     final_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
     # periodoFechasAlquiler(array)
+=======
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, null=True)
+    date = models.DateField(default="1998-7-27", null=True)
+    code = models.CharField(null=True, max_length=50, verbose_name="El codigo de la reserva")
+# TODO decidir el tamaño de las imagenes y ver q se tome el id del logeado en ADMIN para agregar propiedades
+# TODO tmb falta ver como manejar exactamente las fechas
+>>>>>>> Stashed changes
 
 # TODO Falta agregar el array de fechas y de servicios, decidir tamaño de imagenes
